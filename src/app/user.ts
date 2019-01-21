de6bbc706bef85db8fb10a3e3e8018e7c0b18ec9
Watinguyaz <watimakhanu@gmail.com>
2019-01-21 17:02:03 +0300

    home styling done

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,3 +1,4 @@
 export class User {
-  constructor(private username:string,){}
+  constructor(public avatar:any, public name:string ,public followers:number,public following:number,public bio:string,
+    public repos:number, public created_at:string){}
 }
