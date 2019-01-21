edcbb7c551c4607f615bc30e9dad74b1c7f3aaef
Watinguyaz <watimakhanu@gmail.com>
2019-01-21 05:08:41 +0300

    routing functionality set and running

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { GitComponent } from './git/git.component';
+import { SearchComponent } from './search/search.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes:Routes=[
+  {path:"profile",component:ProfileComponent},
+  {path:"search",component:SearchComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    GitComponent
+    GitComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Repository} from '../repository'
+import {User} from '../user'
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
